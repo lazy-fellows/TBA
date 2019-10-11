64b31c54ebf23f70bc1faf05bfbab72fa7f9963d
Jennifer Lai <laij12@uci.edu>
2019-10-11 10:31:23 -0500

    Converting Javascript to Typescript

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-var express = require('express');
-var request = require('request'); // "Request" library
-var cors = require('cors');
-var cookieParser = require('cookie-parser');
+import colors = require('colors');
+import express = require('express');
+import request = require('request'); // "Request" library
+import cors = require('cors');
+import cookieParser = require('cookie-parser');
 
 
 var app = express();
@@ -28,7 +29,6 @@ var generateRandomString = function(length) {
 
 
 app.get('/', function (req, res) {
-    console.log('hereeee');
   res.send('Hello World!');
 });
 
@@ -36,10 +36,8 @@ app.get('/login', function(req, res) {
 
     var state = generateRandomString(16);
     res.cookie(stateKey, state);
-    console.log(state.slice(0, 5));
     // your application requests authorization
     var scope = 'user-read-private user-read-email playlist-read-private';
-    console.log('before redirect');
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
@@ -51,17 +49,13 @@ app.get('/login', function(req, res) {
 });
 
 app.get('/callback', function (req, res) {
-    console.log("In Callback");
     // your application requests refresh and access tokens
     // after checking the state parameter
 
     var code = req.query.code || null;
     var state = req.query.state || null;
-    console.log(req.cookies[stateKey].slice(0, 5));
     var storedState = req.cookies ? req.cookies[stateKey] : null;
     
-    console.log(state === null);
-    console.log(state === storedState);
     if (state === null || state !== storedState) {
         res.redirect('/#' +
             querystring.stringify({
@@ -87,7 +81,6 @@ app.get('/callback', function (req, res) {
 
                 var access_token = body.access_token,
                     refresh_token = body.refresh_token;
-                console.log(body.expires_in);
                 var options = {
                     url: 'https://api.spotify.com/v1/me',
                     headers: { 'Authorization': 'Bearer ' + access_token },
@@ -96,7 +89,6 @@ app.get('/callback', function (req, res) {
 
                 // use the access token to access the Spotify Web API
                 request.get(options, function (error, response, body) {
-                    console.log(body);
                 });
 
                 // we can also pass the token to the browser to make requests from there
@@ -116,5 +108,5 @@ app.get('/callback', function (req, res) {
 });
 
 app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
+    console.log(colors.green(`App listening on port 3000!`));
 });
