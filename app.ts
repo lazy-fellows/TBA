ca59fcc87415f5cc3c663bb91c016ebd0eb12f83
jennellelai <jennelle.lai@gmail.com>
2019-11-02 18:37:35 -0700

    Retrieve Playlists (#5)

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import cors = require('cors');
 import cookieParser = require('cookie-parser');
 
 import { authRouter } from './server/routes/authRoutes';
+import { playlistRouter } from './server/routes/playlistRoutes';
 
 var app = express();
 
@@ -11,6 +12,8 @@ app.use(express.static(__dirname + '/public')).use(cors()).use(cookieParser());
 
 // Routes
 app.use('/', authRouter)
+
+app.use('/', playlistRouter)
 
 app.listen(3000, () => {
     console.log(colors.green(`App listening on port 3000!`));
